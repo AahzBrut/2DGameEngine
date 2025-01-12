7f198f41cc6abfb8c5cc9110639f63aa975705d9
artemenko_pa <artemenko_pa@magnit.ru>
2025-01-12 19:18:25 +0300

    Code cleanup

--- include/components/TransformComponent.h
+++ include/components/TransformComponent.h
@@ -10,9 +10,11 @@ struct TransformComponent : Component<TransformComponent> {
     glm::vec2 scale;
     double rotation;
 
-    explicit TransformComponent(const glm::vec2 position = glm::vec2(0, 0), const glm::vec2 scale = glm::vec2(1, 1), const double rotation = 0.0) {
-        this->position = position;
-        this->scale = scale;
-        this->rotation = rotation;
-    }
+    explicit TransformComponent(
+        const glm::vec2 &position = glm::vec2{0, 0},
+        const glm::vec2 &scale = glm::vec2{1, 1},
+        const double rotation = 0.0)
+        : position{position},
+          scale{scale},
+          rotation{rotation} {}
 };
--- include/systems/CameraMovementSystem.h
+++ include/systems/CameraMovementSystem.h
@@ -18,15 +18,15 @@ public:
 
             const auto posX = transform.position.x;
             const auto posY = transform.position.y;
-            const auto offsetX = sprite.rect.w * transform.scale.x * .5f;
-            const auto offsetY = sprite.rect.h * transform.scale.y * .5f;
-            const auto widthShift = (camera.w >> 1) - offsetX;
-            const auto heightShift = (camera.h >> 1) - offsetY;
-            if (posX > widthShift && posX < Game::mapWidth - widthShift - offsetX * 2) {
-                camera.x = posX - widthShift;
+            const auto offsetX = static_cast<float>(sprite.rect.w) * transform.scale.x * .5f;
+            const auto offsetY = static_cast<float>(sprite.rect.h) * transform.scale.y * .5f;
+            const auto widthShift = static_cast<float>(camera.w >> 1) - offsetX;
+            const auto heightShift = static_cast<float>(camera.h >> 1) - offsetY;
+            if (posX > widthShift && posX < static_cast<float>(Game::mapWidth) - widthShift - offsetX * 2) {
+                camera.x = static_cast<int>(posX - widthShift);
             }
-            if (posY > heightShift && posY < Game::mapHeight - heightShift - offsetY * 2) {
-                camera.y = posY - heightShift;
+            if (posY > heightShift && posY < static_cast<float>(Game::mapHeight) - heightShift - offsetY * 2) {
+                camera.y = static_cast<int>(posY - heightShift);
             }
             camera.x = camera.x < 0 ? 0 : camera.x > camera.w ? camera.w : camera.x;
             camera.y = camera.y < 0 ? 0 : camera.y > camera.h ? camera.h : camera.y;
