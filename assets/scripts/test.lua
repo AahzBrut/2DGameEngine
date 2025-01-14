23ad25518425fcc96e551cc57b4a1539fd84c05d
artemenko_pa <artemenko_pa@magnit.ru>
2025-01-14 18:32:58 +0300

    Add lua and sol

---
+++ assets/scripts/test.lua
@@ -0,0 +1,5 @@
+some_variable = 7 * 6
+
+if some_variable == 42 then
+    print("The value of some_variable: "..some_variable)
+end
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
 #include <SDL2/SDL_main.h>
+#include <sol/sol.hpp>
 #include "core/Game.h"
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
-    Game game;
 
-    game.Initialize();
-    game.Run();
-    game.Destroy();
+void TestLua() {
+    sol::state lua;
+    LOG("TestLua");
+
+    lua.open_libraries(sol::lib::base);
+
+    lua.script_file("./assets/scripts/test.lua");
+
+    int luaVariable = lua["some_variable"];
+
+    LOG("Value of lua variable gets: {}", luaVariable);
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
+    // Game game;
+    //
+    // game.Initialize();
+    // game.Run();
+    // game.Destroy();
+
+    TestLua();
 
     return 0;
 }
